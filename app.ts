1b723e3fdc5c0def49fb3823134a5061e3b8a36b
usama <usamaali052@gmail.com>
2022-11-28 11:03:49 +0500

    array and string type and type alias

--- app.ts
+++ app.ts
@@ -1,13 +1,34 @@
 const n1E = document.getElementById('n1') as HTMLInputElement // this is the typecasting
 const n2E = document.getElementById('n2') as HTMLInputElement
-const btn = document.querySelector('button') // typescript automatically knows that this is a button because we 
+const btn = document.querySelector('button')! // typescript automatically knows that this is a button because we 
 // used the query selector here.
 
-const add = (n1:number, n2:number) => n1 + n2
+const numRes: number[] = []
+const txtRes: string[] = []
+
+type NumOrString = number | string
+
+const add = (n1:NumOrString, n2:NumOrString) => {
+  if(typeof n1 === 'number' && typeof n2 === 'number')
+    return n1 + n2
+  else if(typeof n1 === 'string' && typeof n2 === 'string')
+    return n1 + n2
+  return +n1 + +n2
+}
+
+const printResult = (resObj: {val: number; timestamp: Date}) => {
+  console.log(resObj.val)
+}
+
 if(btn) {
   btn.addEventListener('click', () => {
     const num1 = n1E.value
     const num2 = n2E.value
     const result = add(+num1, +num2)
+    numRes.push(result as number)
+    const stringResult = add('1', '2')
+    txtRes.push(stringResult as string)
+    printResult({val: result as number, timestamp: new Date()})
+    console.log(numRes, txtRes)
   })
 }
